6802712c7a412d82722c7f9d712e4130da99a55a
Colin Kiama <colinkiama@hotmail.co.uk>
2017-12-28 22:45:26 +0000

    Finished main page ui

---
+++ picUp/Model/photo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace picUp.Model
+{
+    public class photo
+    {
+        #region Fields
+
+        public static ObservableCollection<photo> photos = new ObservableCollection<photo>();
+
+        #endregion
+
+        #region Properties
+
+        public BitmapImage ImageSource { get; set; }
+        public StorageFile FileReference { get; set; }
+        public string FileName { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public photo()
+        {
+
+        }
+
+        public photo(string fileName, BitmapImage imageSource, StorageFile fileReference)
+        {
+            FileName = fileName;
+            ImageSource = imageSource;
+            FileReference = FileReference;
+        }
+
+        #endregion
+
+        #region Methods
+
+       
+
+        #endregion
+
+    }
+}
